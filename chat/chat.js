914ce0a018737c5540aa3fd87717fada6e977847
lashachubo <lashachubinidz3@gmail.com>
2024-07-02 16:36:37 +0400

    sss

--- chat/chat.js
+++ chat/chat.js
@@ -1,66 +1,76 @@
 const { uuid } = require("../helpers");
 const Chat = require("../models/Chat");
+const User = require("../models/User");
 
 // TODO 1
-const mapChatParticipants = (chat, userId) => {
-    // you should get participants;
-    // Find participant that not equal to userId
-    // go to user table and get this user name "name"
-    return {
-        ...chat,
-        // name: name
-    }
-}
+const mapChatParticipants = async (chat, userId) => {
+  // you should get participants;
+  // Find participant that not equal to userId
+  // go to user table and get this user name "name"
+  const participantIds = chat.participants.filter((id) => id !== userId);
+  const participants = await User.find(
+    { _id: { $in: participantIds } },
+    "name"
+  );
+  return {
+    ...chat,
+    participants: participants.map((p) => p.name),
+  };
+};
 
 const chatRouter = (app) => {
-    app.get("/api/chats", async (req, res) => {
-        const { userId } = req.query;
-        const response = await Chat.find({}); // TODO 2 we select all chats, but we
-        // need to select only the chat if userId includes in the field participants. Hint: play around $in
-
-        res.send(response.map(item => mapChatParticipants(item, userId)));
+  app.get("/api/chats", async (req, res) => {
+    const { userId } = req.query;
+    // TODO 2 we select all chats, but we
+    // need to select only the chat if userId includes in the field participants. Hint: play around $in
+    const chats = await Chat.find({
+      participants: { $in: [userId] },
     });
 
-    app.post("/api/chats", async (req, res) => {
-        const { userId } = req.query; // TODO 3 we need to pass it from UI
-        const chat = new Chat({
-            participants: req.body.participants,
-            id: uuid(),
-        });
+    const response = await Promise.all(
+      chats.map((chat) => mapChatParticipants(chat, userId))
+    );
+    res.send(response);
+    res.send(response.map((item) => mapChatParticipants(item, userId)));
+  });
 
-        await chat.save();
-        //
-        // All chats related to users;
-        res.send(mapChatParticipants(chat, userId));
+  app.post("/api/chats", async (req, res) => {
+    const { userId } = req.query; // TODO 3 we need to pass it from UI
+    const chat = new Chat({
+      participants: req.body.participants,
+      id: uuid(),
     });
 
-    app.delete("/api/chats", async (req, res) => {
-        const { userId } = req.query;
-        // All chats related to users;
-    });
+    await chat.save();
+    //
+    // All chats related to users;
+    res.send(mapChatParticipants(chat, userId));
+  });
 
-    app.get("/api/messages/:chatId", async (req, res) => {
-        const { chatId } = req.params;
-        // All chats related to users;
-    });
+  app.delete("/api/chats", async (req, res) => {
+    const { userId } = req.query;
+    // All chats related to users;
+  });
 
-    app.post("/api/messages/:chatId", async (req, res) => {
-        const { chatId } = req.params;
-        // All chats related to users;
-    });
+  app.get("/api/messages/:chatId", async (req, res) => {
+    const { chatId } = req.params;
+    // All chats related to users;
+  });
 
-    app.delete("/api/messages/:chatId", async (req, res) => {
-        const { chatId } = req.params;
-        // All chats related to users;
-    });
+  app.post("/api/messages/:chatId", async (req, res) => {
+    const { chatId } = req.params;
+    // All chats related to users;
+  });
 
-    app.patch("/api/messages/:chatId", async (req, res) => {
-        const { chatId } = req.params;
-        // All chats related to users;
-    });
+  app.delete("/api/messages/:chatId", async (req, res) => {
+    const { chatId } = req.params;
+    // All chats related to users;
+  });
 
-
-
+  app.patch("/api/messages/:chatId", async (req, res) => {
+    const { chatId } = req.params;
+    // All chats related to users;
+  });
 };
 
 module.exports = { chatRouter };
